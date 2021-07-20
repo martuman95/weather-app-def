98d0e25505263d5c099bb4c5cd3769f9b64710fa
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-07-20 09:41:45 +0200

    add api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 let apiKey = "1d1742d71e2e4296840a997a3c66e304";
-let apiUrl = 'api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}';
+let apiUrl = 'api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric';
+
+console.log(apiUrl)
