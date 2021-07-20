ae4d058f853be7bb4b9f33795700b40d15f963aa
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-07-20 10:00:54 +0200

    change link

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function dispalyTemperature(response) {
 }
 
 let apiKey = "1d1742d71e2e4296840a997a3c66e304";
-let apiUrl = 'https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric';
+let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric';
 
 console.log(apiUrl)
 axios.get(apiUrl).then(dispalyTemperature);
