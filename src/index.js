3675f2db9edb98744bd7c0e87a0016889f232b55
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-07-20 12:31:02 +0200

    changes style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 
 function dispalyTemperature(response) {
-    console.log(response.data.main.temp);
+console.log(response.data)
+let temperatureElement = document.querySelector("#degrees");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#sky");
+let maxTemp = document.querySelector("#max-temp");
+let minTemp = document.querySelector("#min-temp");
+let feelsLike = document.querySelector("#feels");
+let humidityElement = document.querySelector("#humidity");
+let windSpeed = document.querySelector("#wind");
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+maxTemp.innerHTML = Math.round(response.data.main.temp_max);
+minTemp.innerHTML = Math.round(response.data.main.temp_min);
+feelsLike.innerHTML = `${Math.round( response.data.main.feels_like )} °C`;;
+humidityElement.innerHTML = response.data.main.humidity + " %" ;
+windSpeed.innerHTML = response.data.wind.speed + " Km/h";
 }
 
 let apiKey = "f5ee6fe9739269adb6179e45323cceb3";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl)
+
 axios.get(apiUrl).then(dispalyTemperature);
